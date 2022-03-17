a6b5c0b2d4b017182db23142b5b2e4a6ba46e62a
harshprasad12 <41306515+harshprasad12@users.noreply.github.com>
2022-03-17 17:58:09 +0530

    Added solution - LeetHub

--- Union of two arrays - GFG/union-of-two-arrays.cpp
+++ Union of two arrays - GFG/union-of-two-arrays.cpp
@@ -13,13 +13,17 @@ class Solution{
     int doUnion(int a[], int n, int b[], int m)  {
         //code here
         set<int>s;
+        
         for(int i=0;i<n;i++){
             s.insert(a[i]);
         }
-        for(int i=0;i<m;i++){
-            s.insert(b[i]);
+        
+        for(int j=0;j<m;j++){
+            s.insert(b[j]);
         }
+        
         return s.size();
+        
 }
     
         
